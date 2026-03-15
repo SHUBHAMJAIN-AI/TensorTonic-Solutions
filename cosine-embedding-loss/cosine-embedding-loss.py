dc2f3ab530e6bb60d91b24962e830dc30cbd49fc
shubham jain <37908309+SHUBHAMJAIN-AI@users.noreply.github.com>
2026-03-15 19:44:55 -0400

    Add solution for cosine-embedding-loss

---
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -0,0 +1,16 @@
+def cosine_embedding_loss(x1, x2, label, margin):
+    """
+    Compute cosine embedding loss for a pair of vectors.
+    """
+    import numpy as np
+    x1 = np.asarray(x1)
+    x2 = np.asarray(x2)
+    cosine_prodcut = np.dot (x1,x2)
+    noramlized_product = np.linalg.norm(x1)*np.linalg.norm(x2)
+    cosine_similarity = cosine_prodcut/noramlized_product
+    loss1 = 1-cosine_similarity
+    loss2 = np.maximum(0,cosine_similarity-margin)
+    output = np.where(label==1,loss1,loss2)
+
+    return output.item()
+    
