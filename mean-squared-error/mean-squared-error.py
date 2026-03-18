f4cfbb93ebbf5250efc63fddd55ddf7d1104de9b
shubham jain <37908309+SHUBHAMJAIN-AI@users.noreply.github.com>
2026-03-18 19:09:37 -0400

    Update solution for mean-squared-error

--- mean-squared-error/mean-squared-error.py
+++ mean-squared-error/mean-squared-error.py
@@ -6,5 +6,8 @@ def mean_squared_error(y_pred, y_true):
     """
     y_pred = np.asarray(y_pred)
     y_true = np.asarray(y_true)
+
+    if y_pred.shape != y_true.shape:
+        return None
     loss = np.mean((y_pred-y_true)**2)
     return loss
